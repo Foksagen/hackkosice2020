ab69d434ece802edf5d7b10a178641b3b3352b69
Foksagen <tomasek.matej@gmail.com>
2020-09-05 17:46:07 +0200

    Commit 3

---
+++ lib/courses_search_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class CoursesSearchPage extends StatefulWidget {
+
+  @override
+  CoursesSearchPageState createState() => CoursesSearchPageState();
+}
+
+class CoursesSearchPageState extends State<CoursesSearchPage> {
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
